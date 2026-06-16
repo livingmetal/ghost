02b185398f2eeb2f1f02d80c2017089d56533f81
livingmetal <58518658+livingmetal@users.noreply.github.com>
2026-06-16 14:37:09 +0900

    Add advanced workbench window behavior

---
+++ src/LivingMetalGhost/UI/Views/AdvancedWorkbenchWindow.xaml.cs
@@ -0,0 +1,143 @@
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using LivingMetalGhost.UI.ViewModels;
+
+namespace LivingMetalGhost.UI.Views;
+
+public partial class AdvancedWorkbenchWindow : Window
+{
+    private MainViewModel? _subscribedViewModel;
+
+    public AdvancedWorkbenchWindow()
+    {
+        InitializeComponent();
+        DataContextChanged += AdvancedWorkbenchWindow_OnDataContextChanged;
+        Loaded += (_, _) => SubscribeToMessages(DataContext as MainViewModel);
+    }
+
+    public void FocusPrompt()
+    {
+        PromptTextBox.Focus();
+        Keyboard.Focus(PromptTextBox);
+    }
+
+    public void ShowWorkbench()
+    {
+        if (!IsVisible)
+        {
+            Show();
+        }
+
+        Activate();
+        FocusPrompt();
+    }
+
+    private void AdvancedWorkbenchWindow_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        SubscribeToMessages(e.NewValue as MainViewModel);
+    }
+
+    private void SubscribeToMessages(MainViewModel? viewModel)
+    {
+        if (_subscribedViewModel is not null)
+        {
+            _subscribedViewModel.Messages.CollectionChanged -= Messages_OnCollectionChanged;
+            foreach (var message in _subscribedViewModel.Messages)
+            {
+                message.PropertyChanged -= Message_OnPropertyChanged;
+            }
+        }
+
+        _subscribedViewModel = viewModel;
+        if (_subscribedViewModel is not null)
+        {
+            _subscribedViewModel.Messages.CollectionChanged += Messages_OnCollectionChanged;
+            foreach (var message in _subscribedViewModel.Messages)
+            {
+                message.PropertyChanged += Message_OnPropertyChanged;
+            }
+        }
+
+        ScrollToLatestMessage();
+    }
+
+    private void Messages_OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems is not null)
+        {
+            foreach (var item in e.NewItems.OfType<Core.Models.ChatMessage>())
+            {
+                item.PropertyChanged += Message_OnPropertyChanged;
+            }
+        }
+
+        if (e.OldItems is not null)
+        {
+            foreach (var item in e.OldItems.OfType<Core.Models.ChatMessage>())
+            {
+                item.PropertyChanged -= Message_OnPropertyChanged;
+            }
+        }
+
+        ScrollToLatestMessage();
+    }
+
+    private void Message_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(Core.Models.ChatMessage.Text) or nameof(Core.Models.ChatMessage.DisplayText))
+        {
+            ScrollToLatestMessage();
+        }
+    }
+
+    private void ScrollToLatestMessage()
+    {
+        Dispatcher.BeginInvoke(ConversationScrollViewer.ScrollToEnd);
+    }
+
+    private void Header_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            ExitAdvancedMode();
+            e.Handled = true;
+            return;
+        }
+
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void ExitAdvancedButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        ExitAdvancedMode();
+    }
+
+    private void ExitAdvancedMode()
+    {
+        _subscribedViewModel?.SetAdvancedMode(false);
+    }
+
+    private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Hide();
+    }
+
+    private void PromptTextBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter || Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+        {
+            return;
+        }
+
+        e.Handled = true;
+        if (DataContext is MainViewModel viewModel && viewModel.SendCommand.CanExecute(null))
+        {
+            viewModel.SendCommand.Execute(null);
+        }
+    }
+}
